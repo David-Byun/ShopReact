47505beb61586435060390551d3a5f037be68228
David Byun <byundavid@naver.com>
2023-03-21 07:40:34 +0900

    react 연습

--- src/App.js
+++ src/App.js
@@ -3,26 +3,74 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 import { useState } from 'react';
 import './App.css';
 import data from './data';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import Detail from './routes/Detail';
 
 function App() {
   let [shoes] = useState(data);
+
+  // hook : 유용한 것들이 들어있는 함수
+  // 페이지 이동을 도와주는 함수
+  let navigate = useNavigate();
 
   return (
     <div className="App">
       <Navbar bg="light" variant="light">
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
-          <Nav className="me-auto"></Nav>
+          <Nav className="me-auto">
+            <Nav.Link
+              onClick={() => {
+                navigate('/');
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate('/detail');
+              }}
+            >
+              Detail
+            </Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
-      <div className="main-bg"></div>
-      <div className="container">
-        <div className="row">
-          {shoes.map((a, i) => {
-            return <Card shoes={shoes[i]} i={i}></Card>;
-          })}
-        </div>
-      </div>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <div className="container">
+                <div className="row">
+                  {shoes.map((a, i) => {
+                    return <Card shoes={shoes[i]} i={i}></Card>;
+                  })}
+                </div>
+              </div>
+            </>
+          }
+        />
+        <Route path="/detail" element={<Detail />} />
+
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버임</div>} />
+          <Route path="location" element={<About />} />
+        </Route>
+
+        <Route path="*" element={<div>없는 페이지에요!</div>} />
+      </Routes>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
     </div>
   );
 }
