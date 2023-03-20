b9ae7219dee50b1434ccf01632610e7799b7fe63
David Byun <byundavid@naver.com>
2023-03-21 07:11:54 +0900

    react 연습

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { useState } from 'react';
+import './App.css';
+import data from './data';
+
+function App() {
+  let [shoes] = useState(data);
+
+  return (
+    <div className="App">
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav className="me-auto"></Nav>
+        </Container>
+      </Navbar>
+      <div className="main-bg"></div>
+      <div className="container">
+        <div className="row">
+          {shoes.map((a, i) => {
+            return <Card shoes={shoes[i]} i={i}></Card>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={
+          'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'
+        }
+        width="80%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+      <p>{props.shoes.price}</p>
+    </div>
+  );
+}
+
+export default App;
